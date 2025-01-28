c7b2213b570bc990d69ba40b1469370e46dd8ed9
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-28 15:37:40 +0530

    Created the role grids and add the icons

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import ( 
-	"html/template"  
-    "io" 
-	"github.com/labstack/echo/v4/middleware"
+import (
+	"html/template"
+	"io"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
+
 type Templates struct {
 	templates *template.Template
 }
@@ -13,55 +15,82 @@ type Templates struct {
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
+
 func newTemplate() *Templates {
 	return &Templates{
 		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
-}
-type Count struct {
-	Count int
 }
 
 type Contact struct {
 	Name  string
 	Email string
 }
+
 func newContact(name, email string) *Contact {
 	return &Contact{
 		Name:  name,
 		Email: email,
 	}
 }
+
 type contacts = []Contact
-type Data struct {
-	Contacts contacts
+
+type Role struct {
+	ID       int
+	RoleName string
+	Icon     string
 }
+
+type Data struct {
+	Contacts []Contact
+	Roles    []Role
+}
+
 func newData() *Data {
 	return &Data{
 		Contacts: []Contact{
 			*newContact("Abinesh", "Abinesh@gmail.com"),
 			*newContact("Bobby", "Bobby@gmail.com"),
 		},
+		Roles: []Role{
+			{ID: 1, RoleName: "Admin", Icon: "/icons/admin.png"},
+			{ID: 2, RoleName: "User", Icon: "/icons/user.png"},
+			{ID: 3, RoleName: "Manager", Icon: "/icons/manager.png"},
+		},
 	}
 }
 
 func main() {
 	e := echo.New()
+
+	// Use middleware for logging
 	e.Use(middleware.Logger())
+
+	// Set the template renderer
 	e.Renderer = newTemplate()
+
+	// Sample data
 	data := newData()
+
+	// Handle the root route to render the page
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", data)
+		return c.Render(http.StatusOK, "index", data)
 	})
 
+	// Handle the POST request to add new contacts
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 		newContact := *newContact(name, email)
 		data.Contacts = append(data.Contacts, newContact)
-		return c.Render(200, "newentry", newContact)
+		return c.Render(http.StatusOK, "index", data)
 	})
-	
+
+	// Serve static files for icons and CSS
+	e.Static("/icons", "icons")
+	e.Static("/css", "views/css")
+
+	// Start the server
 	e.Logger.Fatal(e.Start(":42069"))
 }
-
